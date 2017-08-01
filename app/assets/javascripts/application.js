6ff31e1619831e2bd1ee6c2b3c550b6b0ff6c148
scum7ck <danunaxuilol@mail.ru>
2017-08-01 21:10:09 +0300

    Done

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,7 +14,6 @@
 //= require jquery_ujs
 //= require twitter/bootstrap
 //= require rails-ujs
-// require turbolinks
 //= require_tree .
 
 
--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,12 @@ Rails.application.routes.draw do
 
   devise_for :users, :controllers => { :registrations => "user/registrations" }
   resources :user_stocks, except: [:show, :edit, :update]
+  resources :users, only: [:show]
+  resources :friendships
+  get 'search_friends', to: 'users#search'
+  post 'add_friend', to: 'users#add_friend'
   get 'search_stocks', to: 'stocks#search'
-
+  get 'my_friends', to: 'users#my_friends'
 	root 'welcome#index'
   get 'my_portfolio', to: "users#my_portfolio"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
